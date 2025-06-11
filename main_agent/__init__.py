9e34e3a79bb4ec9bc5bf849a74abf6a343e88dc7
爐㶮彪狐居士 <47691322+BiuwuLOK@users.noreply.github.com>
2025-06-11 03:55:33 +0000

    new files

--- main_agent/__init__.py
+++
@@ -1,4 +0,0 @@
-# __init__.py
-from .data_fetcher import get_bars
-from .indicators import get_macd
-from .trade_logic import execute_trade
--- main_agent/__main__.py
+++
@@ -1,5 +0,0 @@
-# __main__.py
-from .run import main
-
-if __name__ == "__main__":
-    main()
